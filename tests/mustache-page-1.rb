4285054ba24e43bf0cc02cb15f971995522ca89a
Sam Rowe <samrowe@deneb.co.uk>
2016-01-23 23:31:45 +0000

    The start of a new branch. Using mustache to compile the website from a set of templates and a set of data.

---
+++ tests/mustache-page-1.rb
@@ -0,0 +1,26 @@
+require 'Mustache'
+
+class MustacheTempate1 < Mustache
+	#Need this page to set the template
+	#https://github.com/mustache/mustache/blob/master/lib/mustache.rb
+	self.template_file = "./mustache-template-1.html"
+
+	def pageTitle
+		return "mustache-template-1.html"
+	end
+
+	def headerText
+		return "A big hello world"
+	end
+
+	def comment
+		return "This is a comment"
+	end
+
+	def css
+		return "<style/>"
+	end
+
+end
+
+puts MustacheTempate1.render()
