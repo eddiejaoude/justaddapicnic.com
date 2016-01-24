7e82e6f6e54a2f42331193b5771392e8d197cdc9
Sam Rowe <samrowe@deneb.co.uk>
2016-01-24 12:43:08 +0000

    more working but still not got comments or css working yet

--- beaches.rb
+++ beaches.rb
@@ -5,7 +5,7 @@ class Beaches < Mustache
 	#Need this page to set the template
 	#https://github.com/mustache/mustache/blob/master/lib/mustache.rb
 	self.template_file = "./templates/beaches.html"
-	@DataHash = {"BeachName"=>"EmptyBeach",
+	@@EmptyDataHash = {"BeachName"=>"EmptyBeach",
  				"BeachCSS"=>"./css/emptyBeach.css",
  				"BeachHeader"=>"This is an empty beach",
  				"Comments"=>
@@ -13,30 +13,43 @@ class Beaches < Mustache
 
 	def loadJSONData jsonFilePath
 		begin
-			@DataHash = JSON.load(File.read(jsonFilePath))
+			@LoadedDataHash = JSON.load(File.read(jsonFilePath))
 		rescue StandardError => ex
 			puts ex.message
 		end
 	end
 
-	def pageTitle
-		return "EmptyBeach"
-		#return @DataHash["BeachName"]
+	def beachTitle
+		if @LoadedDataHash.nil? then
+			return @@EmptyDataHash["BeachName"]
+		else
+			return @LoadedDataHash["BeachName"]
+		end
 	end
 
-	def headerText
-		return "This is an empty beach"
-		#return @DataHash["BeachHeader"]
+	def beachHeader
+		if @LoadedDataHash.nil? then
+			return @@EmptyDataHash["BeachHeader"]
+		else
+			return @LoadedDataHash["BeachHeader"]
+		end
+
 	end
 
-	#def comments
-#		return {"Comment"=>"This is the first comment"}
-		#return @DataHash["Comments"]
-#	end
+	def comments
+		if @LoadedDataHash.nil? then
+			return @@EmptyDataHash["Comments"][0]
+		else
+			return @LoadedDataHash["Comments"]
+		end
+	end
 
-	def css
-		return "./css/emptyBeach.css"
-		#return @DataHash["BeachCss"]
+	def beachCSS
+		if @LoadedDataHash.nil? then
+			return @@EmptyDataHash["beachCSS"]
+		else
+			return @LoadedDataHash["beachCSS"]
+		end
 	end
 
 	def compileToFile compileToFilePath
@@ -48,5 +61,3 @@ class Beaches < Mustache
 		end
 	end
 end
-
-puts Beaches.render()
